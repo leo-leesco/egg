e9c111b9e8fd2f91a7d83f0f4f8c073a54ef0731
Léo LEESCO <leo.leesco@gmail.com>
2025-08-05 13:51:14 -0700

    tests for linear arithmetic

--- tests/linear.rs
+++ tests/linear.rs
@@ -1,15 +1,11 @@
-use std::{
-    collections::BTreeMap,
-    fmt::{Display, Error, Formatter},
-    str::FromStr,
-};
+use std::collections::BTreeMap;
 
-use egg::*;
+use egg::{Rewrite, *};
 
 define_language! {
   enum SimpleMath {
     "+" = Add([Id; 2]),
-    "x" = Mul([Id;2]),
+    "*" = Mul([Id;2]),
     Num(i32),
     Func(Symbol,Vec<Id>),
   }
@@ -30,7 +26,7 @@ impl LinExp {
             *coefs.entry(*sym).or_insert(0) += coef;
         }
         LinExp {
-            coefs,
+            coefs: coefs.into_iter().filter(|(_k, v)| *v != 0).collect(),
             constant: self.constant + other.constant,
         }
     }
@@ -41,8 +37,15 @@ impl LinExp {
                 coefs: other
                     .coefs
                     .iter()
-                    .map(|(k, v)| (*k, *v * self.constant))
-                    .collect::<BTreeMap<Id, i32>>(),
+                    .filter_map(|(k, v)| {
+                        let mul = *v * self.constant;
+                        if mul != 0 {
+                            Some((*k, mul))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect(),
                 constant: self.constant * other.constant,
             })
         } else if other.coefs.iter().all(|(_k, v)| *v == 0) {
@@ -50,8 +53,15 @@ impl LinExp {
                 coefs: self
                     .coefs
                     .iter()
-                    .map(|(k, v)| (*k, *v * other.constant))
-                    .collect::<BTreeMap<Id, i32>>(),
+                    .filter_map(|(k, v)| {
+                        let mul = *v * other.constant;
+                        if mul != 0 {
+                            Some((*k, mul))
+                        } else {
+                            None
+                        }
+                    })
+                    .collect(),
                 constant: self.constant * other.constant,
             })
         } else {
@@ -59,7 +69,7 @@ impl LinExp {
         }
     }
 
-    fn prune(&mut self) {
+    fn _prune(&mut self) {
         self.coefs.retain(|_k, v| *v != 0)
     }
 }
@@ -84,17 +94,78 @@ impl Analysis<SimpleMath> for LinearArith {
             SimpleMath::Mul([a, b]) => x(a)?.mul(&x(b)?),
             SimpleMath::Add([a, b]) => Some(x(a)?.add(&x(b)?)),
             SimpleMath::Func(_f, _args) => Some(LinExp {
-                coefs: std::iter::once((id, 1)).collect::<BTreeMap<_, _>>(),
+                coefs: std::iter::once((id, 1)).collect(),
                 constant: 0,
             }),
         }
     }
 
-    fn modify(egraph: &mut EGraph<SimpleMath, Self>, id: Id) {
-        // let added = egraph.add(egraph[id].data.clone());
-        if let Some(linexp) = &egraph[id].data {
-            let added = egraph.add_expr(&linexp.to_rec_expr());
-            egraph.union(id, added);
+    fn modify(egraph: &mut EGraph<SimpleMath, Self>, eclass: Id) {
+        if let Some(linexp) = &egraph[eclass].data.clone() {
+            let mut expr = linexp.coefs.iter().fold(None, |acc, (id, coef)| {
+                if *coef == 0 {
+                    return acc;
+                }
+
+                let coef_node = egraph.add(SimpleMath::Num(*coef));
+                let mul = egraph.add(SimpleMath::Mul([coef_node, *id]));
+
+                Some(if let Some(prev) = acc {
+                    egraph.add(SimpleMath::Add([prev, mul]))
+                } else {
+                    mul
+                })
+            });
+
+            // Step 2: add the constant if non-zero
+            if linexp.constant != 0 {
+                let const_node = egraph.add(SimpleMath::Num(linexp.constant));
+                expr = Some(if let Some(prev) = expr {
+                    egraph.add(SimpleMath::Add([prev, const_node]))
+                } else {
+                    const_node
+                });
+            }
+
+            // Step 3: union the final expression with the eclass
+            if let Some(expr) = expr {
+                egraph.union(eclass, expr);
+            }
         }
     }
 }
+
+fn rules() -> Vec<Rewrite<SimpleMath, LinearArith>> {
+    vec![
+        rewrite!("comm-add";  "(+ ?a ?b)"        => "(+ ?b ?a)"),
+        rewrite!("comm-mul";  "(* ?a ?b)"        => "(* ?b ?a)"),
+        rewrite!("assoc-add"; "(+ ?a (+ ?b ?c))" => "(+ (+ ?a ?b) ?c)"),
+        rewrite!("assoc-mul"; "(* ?a (* ?b ?c))" => "(* (* ?a ?b) ?c)"),
+        rewrite!("sub-canon"; "(- ?a ?b)" => "(+ ?a (* -1 ?b))"),
+        rewrite!("zero-add"; "(+ ?a 0)" => "?a"),
+        rewrite!("zero-mul"; "(* ?a 0)" => "0"),
+        rewrite!("one-mul";  "(* ?a 1)" => "?a"),
+        rewrite!("add-zero"; "?a" => "(+ ?a 0)"),
+        rewrite!("mul-one";  "?a" => "(* ?a 1)"),
+        rewrite!("cancel-sub"; "(- ?a ?a)" => "0"),
+        rewrite!("distribute"; "(* ?a (+ ?b ?c))"        => "(+ (* ?a ?b) (* ?a ?c))"),
+        rewrite!("factor"    ; "(+ (* ?a ?b) (* ?a ?c))" => "(* ?a (+ ?b ?c))"),
+    ]
+}
+
+test_fn! {
+    math_associate_adds, [
+        rewrite!("comm-add"; "(+ ?a ?b)" => "(+ ?b ?a)"),
+        rewrite!("assoc-add"; "(+ ?a (+ ?b ?c))" => "(+ (+ ?a ?b) ?c)"),
+    ],
+    runner = Runner::default()
+        .with_iter_limit(7)
+        .with_scheduler(SimpleScheduler),
+    "(+ 1 (+ 2 (+ 3 (+ 4 (+ 5 (+ 6 7))))))"
+    =>
+    "(+ 7 (+ 6 (+ 5 (+ 4 (+ 3 (+ 2 1))))))"
+    @check |r: Runner<SimpleMath, ()>| assert_eq!(r.egraph.number_of_classes(), 127)
+}
+
+egg::test_fn! {math_simplify_add, rules(), "(+ x (+ x (+ x x)))" => "(* 4 x)" }
+egg::test_fn! {math_powers, rules(), "(* (pow 2 x) (pow 2 y))" => "(pow 2 (+ x y))"}
